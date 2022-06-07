6175a30f4ef24f0cfcc22fade313c416edee2e1a
Kate Kravchuk <k8jaim@gmail.com>
2022-06-07 23:28:59 +0300

    ДЗ_02 - исправления согласно код ревью

--- js/main.js
+++ js/main.js
@@ -1,45 +1,37 @@
 function getPositiveRandomInt(min, max) { //функция на генерацию случайного целого положительного числа
-  let result;
-
   //делаем проверку, что число "от" в диапазоне положительное
   if (min < 0) {
     const validationPositiveError = `min = ${min} - минимальное значение диапазона должно быть больше или равно 0. Измените минимальное значение.`;
     return validationPositiveError;
-  } else {
-    //делаем проверку, что число "от" меньше числа "до"
-    if (min >= max) {
-      const validationRangeError = `min = ${min} - минимальное значение больше или равно максимальному. Измените минимальное значение.`;
-      return validationRangeError;
-    } else {
-      //проверки пройдены, генерим число
-      min = Math.ceil(min);
-      max = Math.floor(max);
-      result = Math.floor(Math.random() * (max - min + 1)) + min; //включение максимума и минимума
-      return result;
-    }
   }
+  //делаем проверку, что число "от" меньше числа "до"
+  if (min >= max) {
+    const validationRangeError = `min = ${min} - минимальное значение больше или равно максимальному. Измените минимальное значение.`;
+    return validationRangeError;
+  }
+  //проверки пройдены, генерим число
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  const result = Math.floor(Math.random() * (max - min + 1)) + min; //включение максимума и минимума
+  return result;
 }
 
 getPositiveRandomInt(5, 9); //чтобы линтер не ругался
 
 function getPositiveRandomFloat(min, max, numLength) { //функция на генерацию случайного дробного положительного числа
-  let result;
   //делаем проверку, что число "от" в диапазоне положительное
-
   if (min < 0) {
     const validationPositiveError = `min = ${min} - минимальное значение диапазона должно быть больше или равно 0. Измените минимальное значение.`;
     return validationPositiveError;
-  } else {
-    //делаем проверку, что число "от" меньше числа "до"
-    if (min >= max) {
-      const validationRangeError = `min = ${min} - минимальное значение больше или равно максимальному. Измените минимальное значение.`;
-      return validationRangeError;
-    } else {
-      //проверки пройдены, генерим число
-      result = Math.random() * (max - min + 1) + min; //Максимум и минимум включаются
-      return result.toFixed(numLength);
-    }
   }
+  //делаем проверку, что число "от" меньше числа "до"
+  if (min >= max) {
+    const validationRangeError = `min = ${min} - минимальное значение больше или равно максимальному. Измените минимальное значение.`;
+    return validationRangeError;
+  }
+  //проверки пройдены, генерим число
+  const result = Math.random() * (max - min + 1) + min; //Максимум и минимум включаются
+  return result.toFixed(numLength);
 }
 
 getPositiveRandomFloat(0.345, 6.789467, 4); //чтобы линтер не ругался
